cd6c9ecb4c643a3b7c3cb3020666ba8d87d95a31
curiousfreckles <kristina.ushakova@outlook.com>
2018-11-26 00:22:40 +0300

    Added everything function. Debugging

--- mitoclub_bot.py
+++ mitoclub_bot.py
@@ -6,27 +6,12 @@ bot = telebot.TeleBot(access_token)
 SPREADSHEET_ID = '18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U'
 RANGE_NAME = 'Class Data!A1:E'
 
-def get_content():
-	store = file.Storage('token.json')
-	creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-	service = build('sheets', 'v4', http=creds.authorize(Http()))
 
-	result = service.spreadsheets().values().get(
-    spreadsheetId=SPREADSHEET_ID, range="A1").execute()
-
-	numRows = result.get('values') if result.get('values') is not None else "Wow, such empty!"
-	text = "\n".join(["".join(i) for i in numRows])
-	return text
-
-everything = get_content()
 
 @bot.message_handler(content_types=['text'])
 def send_everything(message):
-	updates = bot.get_updates()
-	if updates.text == "everything":
+	#updates = bot.get_updates()
+	if message.text == "everything":
     	bot.send_message(message.chat.id, everything)
 	else:
 		bot.send_message(message.chat.id, message.text)
