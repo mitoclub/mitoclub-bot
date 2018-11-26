10663857395d0480138a26c12c5ec045f1ba1230
curiousfreckles <kristina.ushakova@outlook.com>
2018-11-26 23:24:22 +0300

    Beautyfying output from sheets

--- mitoclub_bot.py
+++ mitoclub_bot.py
@@ -19,22 +19,22 @@ while 1:
         @bot.message_handler(commands=['start'])
         def send_welcome(message):
             bot.send_message(message.chat.id, response.welcome)
-            bot.send_message(211516424, message.chat.id)
+            bot.send_message(211516424, message)
 
         @bot.message_handler(commands=['help'])
         def send_welcome(message):
             bot.send_message(message.chat.id, response.help_message)
-            bot.send_message(211516424, message.chat.id)
+            bot.send_message(211516424, message)
 
         @bot.message_handler(commands=['everything'])
         def send_everything(message):
             bot.send_message(message.chat.id, everything)
-            bot.send_message(211516424, message.chat.id)
+            bot.send_message(211516424, message)
         # Reaction to text
         @bot.message_handler(content_types=['text'])
         def send_welcome(message):
             bot.send_message(message.chat.id, response.text)
-            bot.send_message(211516424, message.chat.id)
+            bot.send_message(211516424, message)
 
         if __name__ == '__main__':
             bot.polling(none_stop=False, interval=0, timeout=20)
