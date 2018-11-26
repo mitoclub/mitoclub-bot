fa393ab9be1a70252fe24610d1956e4418f6c05e
curiousfreckles <kristina.ushakova@outlook.com>
2018-11-26 21:38:29 +0300

    Google sheets working

---
+++ response.py
@@ -0,0 +1,6 @@
+"""
+	This is the set of responses for the bot
+"""
+
+
+welcome = "Hello, I am the Mitoclub TeleBot! I am responsible for keeping a list of upcoming meetings as well as keeping you posted about the deadlines. All of the meetings are listed here: https://bit.ly/2yCFQ7A \n\n Here's a list of commands I can handle:\n\n/start display the welcome message again\n/everything print all of the upcoming meetings"
