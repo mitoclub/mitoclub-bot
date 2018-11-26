765de701ee4dfee084da36cca0ce689271a9b37a
curiousfreckles <kristina.ushakova@outlook.com>
2018-11-26 21:47:15 +0300

    Google sheets working, fixed the link

--- response.py
+++ response.py
@@ -3,4 +3,4 @@
 """
 
 
-welcome = "Hello, I am the Mitoclub TeleBot! I am responsible for keeping a list of upcoming meetings as well as keeping you posted about the deadlines. All of the meetings are listed here: https://bit.ly/2yCFQ7A \n\n Here's a list of commands I can handle:\n\n/start display the welcome message again\n/everything print all of the upcoming meetings"
+welcome = "Hello, I am the Mitoclub TeleBot! I am responsible for keeping a list of upcoming meetings as well as keeping you posted about the deadlines. All of the meetings are listed here: https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing \n\n Here's a list of commands I can handle:\n\n/start display the welcome message again\n/everything print all of the upcoming meetings"
