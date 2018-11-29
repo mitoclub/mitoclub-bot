7549aa49467a1855dd066d613585a125652976c3
curiousfreckles <kristina.ushakova@outlook.com>
2018-11-29 11:48:53 +0300

    Adding reminders

--- response.py
+++ response.py
@@ -7,5 +7,5 @@ welcome = "Hello, I am the Mitoclub TeleBot! I am responsible for keeping a list
 down = "Service will be down for the next 2 hours due to maintenance. Meetings will be accessible at https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing"
 text = "I can't understand simple text yet, but there is a list of commands I can interpret, access it through the /help command."
 help_message = "All of the meetings are listed here: https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing \n\n Here's a list of commands I can handle:\n\n/start display the welcome message again\n/everything print all of the upcoming meetings"
-reminder_jc = "Just a friendly reminder that Journal Club will begin at 17:00 Kaliningrad time, don't be late! Here's the Discord chat link:\n\nhttps://discord.gg/ufx62AK"
+reminder_jc = "Just a friendly reminder that Journal Club will begin at 17:00 Kaliningrad time, don't be late! Today we are discussing mitochondrial transcription regulation and the mathematics of cause and effect! Join here: \nhttps://discord.gg/ufx62AK"
 reminder_meeting = "Just a friendly reminder that Lab Meeting will begin at 17:00 Kaliningrad time, don't be late! Here's the Discord chat link:\n\nhttps://discord.gg/ufx62AK"
