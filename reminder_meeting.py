3b40664a3339523f3a7cbd335f4d01f7b95b35ac
curiousfreckles <kristina.ushakova@outlook.com>
2018-12-21 16:30:50 +0300

    Meeting remind

---
+++ reminder_meeting.py
@@ -0,0 +1,10 @@
+import telebot
+import response
+from config import bot_access_token
+
+"""
+	JC_reminder task. Runs at 13:30 UTC every Thursday.
+"""
+
+bot = telebot.TeleBot(bot_access_token)
+bot.send_message(-274401487, response.reminder_meeting)
