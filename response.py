eec7986cbf4bb63c1c3f45a8924a0eae3889c93b
curiousfreckles <kristina.ushakova@outlook.com>
2018-12-27 17:16:28 +0300

    Changed response jc

--- response.py
+++ response.py
@@ -4,8 +4,8 @@
 
 
 welcome = "Hello, I am the Mitoclub TeleBot! I am responsible for keeping a list of upcoming meetings as well as keeping you posted about the deadlines. All of the meetings are listed here: https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing \n\n Here's a list of commands I can handle:\n\n/start display the welcome message again\n/everything print all of the upcoming meetings"
-down = "Service will be down for the next 2 hours due to maintenance. Meetings will be accessible at https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing"
+down = "Service will be down for a while due to maintenance. Meetings will be accessible at https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing"
 text = "I can't understand simple text yet, but there is a list of commands I can interpret, access it through the /help command."
 help_message = "All of the meetings are listed here: https://docs.google.com/spreadsheets/d/18c-03qAYD0RELLXv1b7G-dJkPgFQOCI2jP19CCh0a2U/edit?usp=sharing \n\n Here's a list of commands I can handle:\n\n/start display the welcome message again\n/everything print all of the upcoming meetings"
-reminder_jc = "Just a friendly reminder that Journal Club will begin at 17:00 Kaliningrad time, don't be late! Join here: \nhttps://discord.gg/ufx62AK"
+reminder_jc = "Just a friendly reminder that Journal Club will begin at 17:00 Kaliningrad time! Is everybody ready? Join here: \nhttps://discord.gg/ufx62AK"
 reminder_meeting = "Just a friendly reminder that Lab Meeting will begin at 17:00 Kaliningrad time, don't be late! Join here:\nhttps://join.skype.com/mwi6jK8Rxrip"
